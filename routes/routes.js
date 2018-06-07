0380d9af998074b62f5de19a57a4c2bba70e3196
crestwood204 <andong@sas.upenn.edu>
2018-06-07 10:23:13 -0400

    configured for rothman server

--- routes/routes.js
+++ routes/routes.js
@@ -230,12 +230,12 @@ var sendEmail = function(to, subject, text) {
 
   // create reusable transporter object using the default SMTP transport
     let transporter = nodemailer.createTransport({
-        host: 'smtp.ethereal.email',
-        port: 587,
-        auth: {
-            user: process.env.ETHEREAL_USERNAME,
-            pass: process.env.ETHEREAL_PASSWORD
-        }
+        host: '10.10.1.72',
+        port: 465//,
+        // auth: {
+        //     user: process.env.ETHEREAL_USERNAME,
+        //     pass: process.env.ETHEREAL_PASSWORD
+        // }
     });
 
     // setup email data with unicode symbols
@@ -253,9 +253,10 @@ var sendEmail = function(to, subject, text) {
         if (error) {
             return console.log('err', error);
         }
+        console.log(info)
         console.log('Message sent: %s', info.messageId);
         // Preview only available when sending through an Ethereal account
-        console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+      //  console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
 
         // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
         // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
