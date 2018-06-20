a53f9f21e8b43bed3c3b5061d31dfed31e46a167
crestwood204 <andong@sas.upenn.edu>
2018-06-20 14:15:07 -0400

    fixed data-toggle async problems

--- public/js/collapsible_table.js
+++ public/js/collapsible_table.js
@@ -1,10 +1,16 @@
 /* eslint-env jquery */
 $(document).ready(() => {
+  const buttonDict = {};
   $('.toggle-row').on('click', function toggleCollapse() {
     const id = $(this).attr('data-target');
+    if (buttonDict.id) {
+      return;
+    }
     if ($(id).hasClass('show')) {
       $(id).parent().addClass('blank-row');
       $(id).parent().siblings(0).addClass('blank-row');
+      buttonDict.id = true;
+      setTimeout(() => { buttonDict.id = false; }, 400);
     } else {
       $(id).parent().removeClass('blank-row');
       $(id).parent().siblings(0).removeClass('blank-row');
