c3d745fa8ca83de32f225bf5df671f29b5a0f258
crestwood204 <andong@sas.upenn.edu>
2018-07-05 11:55:11 -0400

    updated table_search so that pending_requests does not navigate to home view

--- public/js/table_search.js
+++ public/js/table_search.js
@@ -2,6 +2,6 @@ $(document).ready(() => {
   $('#search-btn').on('click', (event) => {
     event.preventDefault();
     // add front end validation like server-side for non [^a-zA-Z0-9] input
-    window.location.href = `/?search=${$('#search').val()}&page=1`;
+    window.location.href = `${$('#search-bar-form').attr('action')}search=${$('#search').val()}&page=1`;
   });
 });
