6f52c7572d897daf8edfdc3622d8b3e87f466b60
crestwood204 <andong@sas.upenn.edu>
2018-07-05 11:39:53 -0400

    active navbar is correct even with query

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,6 @@
 $(document).ready(() => {
   (function setActiveTab() {
-    const url = window.location.href.trim().split('/');
+    const url = window.location.href.trim().split('?')[0].split('/');
     const endpoint = url[url.length - 1];
     if (endpoint === '') {
       $('#home_page').addClass('active');
