554369e4eda31122b588e296d8b893867cd72ef6
crestwood204 <andong@sas.upenn.edu>
2018-07-16 15:44:14 -0400

    fixed deletion of groups/templates

--- public/js/delete_element.js
+++ public/js/delete_element.js
@@ -1,10 +1,15 @@
 $(document).ready(() => {
-  $('.trash').on('click', (event) => {
+  $('.trash').on('click', function deleteElement(event) {
     event.preventDefault();
+    const id = $(this).attr('id').split('-')[2];
     const url = `/delete_${$('#type').text().trim()}`;
     $.ajax({
       url,
       method: 'PUT',
+      data: {
+        id,
+        name: $(this).attr('name')
+      },
       error(err) {
         console.log('error communicating with server', err);
       },
